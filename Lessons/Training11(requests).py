6fe4fc97ca2fef75e8c74f37c5278b313aa1116b
M-Toha <124435879+M-Toha@users.noreply.github.com>
2023-04-22 20:24:09 +0300

    Homework 10

--- Lessons/Training11(requests).py
+++ Lessons/Training11(requests).py
@@ -2,3 +2,4 @@ import requests
 res_monobank = requests.get('https://api.monobank.ua/bank/currency')
 res_monobank.status_code
 print(res_monobank.status_code, type(res_monobank.status_code))
+print(res_monobank.json()[:5:])
